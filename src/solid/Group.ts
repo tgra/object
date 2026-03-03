f57ab58293b4a491631695c3777e53c5b1f7d710
Tanya Gray <tgratgra@gmail.com>
2026-03-03 10:40:14 +0000

    Update src/solid/Group.ts
    Co-authored-by: Samu Lang <langsamu@users.noreply.github.com>

--- src/solid/Group.ts
+++ src/solid/Group.ts
@@ -15,30 +15,4 @@ export class Group extends TermWrapper {
     get members(): Set<Person> {
         return this.objects(VCARD.member, ObjectMapping.as(Person), ObjectMapping.as(Person))
     }
-
-    /** Add a new Person to this group */
-    addMember(person: Person) {
-        // Convert Person term to string IRI
-        const iri = person.term.value
-
-        // Use objects() to get the live set and add the new member
-        const membersSet = this.objects(
-            VCARD.member,
-            ValueMappings.iriToString,
-            TermMappings.stringToIri
-        )
-        membersSet.add(iri)
-    }
-
-    /** Remove a Person from this group */
-    deleteMember(person: Person) {
-        const iri = person.term.value
-
-        const membersSet = this.objects(
-            VCARD.member,
-            ValueMappings.iriToString,
-            TermMappings.stringToIri
-        )
-        membersSet.delete(iri)
-    }
 }
