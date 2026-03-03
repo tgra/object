d1d37dccdc14bee899cbc5696769fe589d90de73
Tanya Gray <tgratgra@gmail.com>
2026-03-03 10:40:55 +0000

    Update src/vocabulary/solid.ts
    Co-authored-by: Samu Lang <langsamu@users.noreply.github.com>

--- src/vocabulary/solid.ts
+++ src/vocabulary/solid.ts
@@ -6,14 +6,10 @@ export const SOLID = {
   preferredRelativePronoun: "http://www.w3.org/ns/solid/terms#preferredRelativePronoun",
   publicId: "http://www.w3.org/ns/solid/terms#publicId",
 
-
   // the following terms are not defined but are present in https://github.com/SolidOS/profile-pane/blob/main/src/ontology/profileForm.ttl
   Role: "http://www.w3.org/ns/solid/terms#Role",
   CurrentRole: "http://www.w3.org/ns/solid/terms#CurrentRole",
   FormerRole: "http://www.w3.org/ns/solid/terms#FormerRole",
   FutureRole: "http://www.w3.org/ns/solid/terms#FutureRole",
-
-  
-
-}
+} as const
 
