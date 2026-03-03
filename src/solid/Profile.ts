95904280c268ed778f7774b560925df01856f2d6
Tanya Gray <tgratgra@gmail.com>
2026-03-03 11:00:38 +0000

    Update src/solid/Profile.ts
    Co-authored-by: Samu Lang <langsamu@users.noreply.github.com>

--- src/solid/Profile.ts
+++ src/solid/Profile.ts
@@ -4,21 +4,21 @@ import { Person } from "./Person.js"
 
 
 export class Profile extends TermWrapper {
-
   /* Nickname */
   get nickname(): string | undefined {
     return this.singularNullable(FOAF.nick, ValueMappings.literalToString)
   }
+
   set nickname(value: string | undefined) {
     this.overwriteNullable(FOAF.nick, value, TermMappings.stringToLiteral)
   }
 
   /* Pronouns */
-
   // Preferred pronoun for subject role (he/she/they
   get preferredSubjectPronoun(): string | undefined {
     return this.singularNullable(SOLID.preferredSubjectPronoun, ValueMappings.literalToString)
   }
+
   set preferredSubjectPronoun(value: string | undefined) {
     this.overwriteNullable(SOLID.preferredSubjectPronoun, value, TermMappings.stringToLiteral)
   }
@@ -27,6 +27,7 @@ export class Profile extends TermWrapper {
   get preferredObjectPronoun(): string | undefined {
     return this.singularNullable(SOLID.preferredObjectPronoun, ValueMappings.literalToString)
   }
+  
   set preferredObjectPronoun(value: string | undefined) {
     this.overwriteNullable(SOLID.preferredObjectPronoun, value, TermMappings.stringToLiteral)
   }
@@ -35,21 +36,14 @@ export class Profile extends TermWrapper {
   get preferredRelativePronoun(): string | undefined {
     return this.singularNullable(SOLID.preferredRelativePronoun, ValueMappings.literalToString)
   }
+
   set preferredRelativePronoun(value: string | undefined) {
     this.overwriteNullable(SOLID.preferredRelativePronoun, value, TermMappings.stringToLiteral)
   }
 
-
   /* Roles / Organization involvement  */
-
   get roles(): Set<Person> {
-    const persons = new Set<Person>()
-
-    for (const iri of this.objects(ORG.member, ValueMappings.iriToString, TermMappings.stringToIri)) {
-      const person = new Person(iri, this.dataset, this.factory)
-      persons.add(person)
-    }
-    return persons
+    return this.objects(ORG.member, ObjectMapping.as(Person), ObjectMapping.as(Person))
   }
 
   /* Skills */
